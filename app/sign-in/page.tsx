9fe73ee5794070a1e06f81aabc8c8a9fce157d09
Sahil <sahilsharma2000dtc@gmail.com>
2024-11-03 16:05:54 +0530

    added signIn page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import React, { useState } from "react";
 import { useSignIn } from "@clerk/nextjs";
@@ -18,43 +18,111 @@ import { Eye, EyeOff } from "lucide-react";
 import { Alert, AlertDescription } from "@/components/ui/alert";
 
 function SignIn() {
-    const [emailAddress, setEmailAddress] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const {isLoaded, setActive, signIn} = useSignIn();
-    const router = useRouter();
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const { isLoaded, setActive, signIn } = useSignIn();
+  const router = useRouter();
 
-    if(!isLoaded){
-        return null;
+  if (!isLoaded) {
+    return null;
+  }
+
+  async function submit(e: React.FormEvent) {
+    e.preventDefault();
+    if (!isLoaded) {
+      return null;
     }
+    try {
+      const result = await signIn.create({
+        identifier: emailAddress,
+        password,
+      });
 
-    async function submit(e: React.FormEvent){
-        e.preventDefault();
-        if(!isLoaded){
-            return null;
-        }
-        try {
-            const result =await signIn.create({
-                identifier: emailAddress, password
-            });
-
-            if(result.status === "complete"){
-                await setActive({
-                    session: result.createdSessionId
-                });
-                router.push("/dashboard");
-            } else {
-                console.log(JSON.stringify(result, null, 2));
-            }
-        } catch (error: any) {
-            console.error("Error: ", error.errors[0].message);
-            setError(error.errors[0].message)
-        }
+      if (result.status === "complete") {
+        await setActive({
+          session: result.createdSessionId,
+        });
+        router.push("/dashboard");
+      } else {
+        console.log(JSON.stringify(result, null, 2));
+      }
+    } catch (error: any) {
+      console.error("Error: ", error.errors[0].message);
+      setError(error.errors[0].message);
     }
+  }
 
   return (
-    <div>SignIn</div>
-  )
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-background">
+        <Card className="w-full max-w-md">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold text-center">
+              Sign In To Your Account
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={submit}>
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  type="email"
+                  id="email"
+                  value={emailAddress}
+                  onChange={(e) => setEmailAddress(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <div className="relative">
+                  <Input
+                    type="password"
+                    id="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <Button
+                    type="button"
+                    onClick={() => setShowPassword(!showPassword)}
+                    className="absolute right-2 top-1/2 -translate-y-1/2"
+                  >
+                    {showPassword ? (
+                      <EyeOff className="h-4 w-4 text-gray-500" />
+                    ) : (
+                      <Eye className="h-4 w-4 text-gray-500" />
+                    )}
+                  </Button>
+                </div>
+              </div>
+              {error && (
+                <Alert variant="destructive">
+                  <AlertDescription>{error}</AlertDescription>
+                </Alert>
+              )}
+              <Button className="w-full" type="submit">
+                SignIn
+              </Button>
+            </form>
+          </CardContent>
+          <CardFooter className="justify-center">
+            <p className="text-sm text-muted-foreground">
+              Don&apos;t have an account?{" "}
+              <Link
+                href="/sign-up"
+                className="font-medium text-primary hover:underline"
+              >
+                SignUp
+              </Link>
+            </p>
+          </CardFooter>
+        </Card>
+      </div>
+    </>
+  );
 }
 
-export default SignIn
+export default SignIn;
