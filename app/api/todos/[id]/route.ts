6e6038a042d1d353b9a430fa4ef3e43f1d4e70fb
Sahil <sahilsharma2000dtc@gmail.com>
2024-11-04 22:22:48 +0530

    completed backend routes for todos

---
+++ app/api/todos/[id]/route.ts
@@ -0,0 +1,93 @@
+//route for update and delete todo
+
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function DELETE(req: NextRequest, {params}:{params: {id: string}}) {
+    const {userId} = auth();
+
+    if(!userId){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+
+    try {
+        const todoId = params.id;
+
+        const todo = await prisma.todo.findUnique({
+            where: {
+                id:  todoId,
+            }
+        });
+
+        if(!todo){
+            return NextResponse.json({error: "Todo not found"}, {status: 404});
+        }
+
+        //if the todo is not created by the logged in user
+        if(todo.userId !== userId){
+            return NextResponse.json({error: "Forbidden"}, {status: 403});
+        }
+
+        await prisma.todo.delete({
+            where: {
+                id: todoId,
+            }
+        });
+
+        return NextResponse.json({message: "Todo deleted successfully"}, {status: 200});
+    } catch (error: any) {
+        console.error("Error deleting todo", error);
+        return NextResponse.json({error: "Something went wrong"}, {status: 500});
+    }
+}
+
+
+export async function PUT(req: NextRequest, {params}: {params: {id: string}}) {
+    const {userId} = auth();
+
+    if(!userId){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+
+    try {
+        const todoId = params.id;
+
+        //finding the todo
+        const todo = await prisma.todo.findUnique({
+            where: {
+                id: todoId,
+            }
+        });
+        
+        if(!todo){
+            return NextResponse.json({error: "Todo not found"}, {status: 404});
+        }
+
+        //if the todo is not created by the logged in user
+        if(todo.userId !== userId){
+            return NextResponse.json({error: "Forbidden"}, {status: 403});
+        }
+
+        //updating the todo
+        const body = await req.json();
+        const {title, completed} = body;
+
+        const updatedTodo = await prisma.todo.update({
+            where: {
+                id: todoId,
+            },
+            data: {
+                title,
+                completed,
+            }
+        });
+
+        return NextResponse.json(updatedTodo, {status: 200});
+    } catch (error: any) {
+        console.error("Error updating todo", error);
+        return NextResponse.json({error: "Internal Server Error"}, {status: 500});
+    }
+}
---
+++ app/api/todos/route.ts
@@ -0,0 +1,102 @@
+//todos route
+
+import prisma from "@/lib/prisma";
+import { auth } from "@clerk/nextjs/server";
+import { error } from "console";
+import { NextRequest, NextResponse } from "next/server";
+
+
+const ITEMS_PER_PAGE = 10;
+
+export async function GET(req: NextRequest){
+    const {userId} = auth();
+
+    if(!userId){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+
+    const {searchParams} = new URL(req.url);
+    const page = parseInt(searchParams.get("page") || "1");
+    const search = searchParams.get("search") || "";
+
+    try {
+        const todos = await prisma.todo.findMany({
+            where: {
+                id: userId,
+                title: {
+                    contains: search,
+                    mode: "insensitive",//case sensitive
+                }
+            },
+            orderBy: {
+                createdAt: "desc"
+            },
+            take: ITEMS_PER_PAGE,
+            skip: (page - 1) * ITEMS_PER_PAGE,
+        });
+
+        const totalItems = await prisma.todo.count({
+            where: {
+                id: userId,
+                title: {
+                    contains: search,
+                    mode: "insensitive",
+                }
+            },
+        });
+
+
+        const totalPages = Math.ceil(totalItems / ITEMS_PER_PAGE);
+        return NextResponse.json({
+            todos,
+            currentPage: page,
+            totalPages
+        });
+
+    } catch (error: any) {
+        console.error("Error fetching todos:", error);
+        return NextResponse.json({error: "Internal server Error."}, {status: 500});
+    }
+}
+
+//adding new todo
+export async function POST(req: NextRequest){
+    const {userId} = auth();
+
+    if(!userId){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+    }
+
+    const user = await prisma.user.findUnique({
+        where: {
+            id: userId,
+        },
+        include: {
+            todos: true,
+        }
+    });
+
+    console.log(user); //for debugging
+
+    if(!user){
+        return NextResponse.json({error: "User not found"}, {status: 404});
+    }
+
+
+    //if user is not subcriber
+    if(!user.isSubscribed && user.todos.length >= 3){
+        //limit exhaustion
+        return NextResponse.json({error: "Free Users can only have 3 todos, please subscribe"}, {status: 403});
+    }
+
+    const {title} = await req.json();
+
+    const newTodo = await prisma.todo.create({
+        data: {
+            title,
+            userId,
+        }
+    });
+
+    return NextResponse.json(newTodo, {status: 201});
+}
